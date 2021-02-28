dcee5fa90cd2464847c4c33009af8979b496f3ba
destroylord <masapin68@gmail.com>
2021-03-01 00:57:54 +0700

    laravel-mix and migration

---
+++ database/migrations/2021_02_28_121903_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('employee_name', 100);
+            $table->enum('gender', ['male', 'female']);
+            $table->string('postion');
+            $table->enum('active', [0,1]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
